93e03727abcccb83b47dfcb873dd2992577c9680
Alexey Pasmarnov <a.pasmarnov@gmail.com>
2019-10-01 23:17:39 +0200

    Moved mouseMoveHandler & mouseUpHandler from init function

--- js/dialog.js
+++ js/dialog.js
@@ -1,18 +1,20 @@
 'use strict';
 
 (function () {
-  var KEY_CODE = {
-    Esc: 27,
-    Enter: 13
+  var KeyCode = {
+    ESC: 27,
+    ENTER: 13
   };
   var userDialog = document.querySelector('.setup');
   var userAvatar = document.querySelector('.setup-open');
   var similarListElement = document.querySelector('.setup-similar-list');
   var closeWizardSetupWindow = document.querySelector('.setup-close');
   var dialogHandle = document.querySelector('.upload');
+  var startCoords = {};
+  var dragged = false;
 
   var onPopupEscPress = function (evt) {
-    if (evt.keyCode === KEY_CODE.Esc) {
+    if (evt.keyCode === KeyCode.ESC) {
       wizardSetupCloseHandler();
     }
   };
@@ -27,16 +29,49 @@
     document.removeEventListener('keydown', onPopupEscPress);
   };
 
+  var mouseMoveHandler = function (moveEvt) {
+    moveEvt.preventDefault();
+    dragged = true;
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    userDialog.style.left = (userDialog.offsetLeft - shift.x) + 'px';
+    userDialog.style.top = (userDialog.offsetTop - shift.y) + 'px';
+  };
+
+  var mouseUpHandler = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', mouseMoveHandler);
+    document.removeEventListener('mouseup', mouseUpHandler);
+
+    if (dragged) {
+      var clickPreventDefaultHandler = function (evtClick) {
+        evtClick.preventDefault();
+        userDialog.removeEventListener('click', clickPreventDefaultHandler);
+      };
+      userDialog.addEventListener('click', clickPreventDefaultHandler);
+    }
+  };
+
   var init = function () {
     similarListElement.appendChild(window.appendSimilarWizard(window.getWizardsData()));
     userAvatar.addEventListener('keydown', function (evt) {
-      if (evt.keyCode === KEY_CODE.Enter) {
+      if (evt.keyCode === KeyCode.ENTER) {
         wizardSetupOpenHandler();
       }
     });
 
     closeWizardSetupWindow.addEventListener('keydown', function (evt) {
-      if (evt.keyCode === KEY_CODE.Enter) {
+      if (evt.keyCode === KeyCode.ENTER) {
         wizardSetupCloseHandler();
       }
     });
@@ -52,44 +87,9 @@
     dialogHandle.addEventListener('mousedown', function (evt) {
       evt.preventDefault();
 
-      var startCoords = {
+      startCoords = {
         x: evt.clientX,
         y: evt.clientY
-      };
-
-      var dragged = false;
-
-      var mouseMoveHandler = function (moveEvt) {
-        moveEvt.preventDefault();
-        dragged = true;
-
-        var shift = {
-          x: startCoords.x - moveEvt.clientX,
-          y: startCoords.y - moveEvt.clientY
-        };
-
-        startCoords = {
-          x: moveEvt.clientX,
-          y: moveEvt.clientY
-        };
-
-        userDialog.style.left = (userDialog.offsetLeft - shift.x) + 'px';
-        userDialog.style.top = (userDialog.offsetTop - shift.y) + 'px';
-      };
-
-      var mouseUpHandler = function (upEvt) {
-        upEvt.preventDefault();
-
-        document.removeEventListener('mousemove', mouseMoveHandler);
-        document.removeEventListener('mouseup', mouseUpHandler);
-
-        if (dragged) {
-          var clickPreventDefaultHandler = function (evtClick) {
-            evtClick.preventDefault();
-            userDialog.removeEventListener('click', clickPreventDefaultHandler);
-          };
-          userDialog.addEventListener('click', clickPreventDefaultHandler);
-        }
       };
 
       document.addEventListener('mousemove', mouseMoveHandler);
